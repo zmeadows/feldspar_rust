a5956922dd712a3606b5e57b9966e53a5731b089
zmeadows <zmeadows@gmail.com>
2018-06-03 17:16:42 -0400

    fixed king attack table bug

--- src/main.rs
+++ src/main.rs
@@ -41,7 +41,7 @@ fn main() {
 
     let mut g = Game::from_fen("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1").unwrap();
 
-    perft(g.clone(), 5);
+    qperft_debug(g.clone());
 
     if false {
         g.board.print();
